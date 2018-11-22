816d5d9c4fa8b2d8f89a17753b5f1c97bf865de5
jurinva <jurinva@gmail.com>
2018-11-22 13:11:17 +0300

    Add colored echo

--- system.sh
+++ system.sh
@@ -2,7 +2,7 @@
 
 function Uinstall() {
 # Install Tools
-  echo "install Tools"
+  echo "install Tools \033[0m"
   sudo apt-get -y install \
   whois wireshark-qt mc traceroute nmap fping iperf iperf3 zenmap p0f \            # network tools
   ansible sshpass remmina vinagre curl snmp \                                      # remote tools
@@ -23,52 +23,52 @@ function Uinstall() {
   sudo apt install celestia-common-nonfree
 
 # Install Lazarus
-  echo "Install Lazarus"
+  echo -en "\033[37;1;41m Install Lazarus \033[0m"
   sudo apt-get install libgtk2.0-dev
   curl --progress-bar -L -o/tmp/lazarus.deb "https://datapacket.dl.sourceforge.net/project/lazarus/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/lazarus-project_1.8.4-0_amd64.deb" && sudo dpkg -i /tmp/lazarus.deb
   curl --progress-bar -L -o/tmp/fpc.deb "https://datapacket.dl.sourceforge.net/project/lazarus/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc_3.0.4-3_amd64.deb" && sudo dpkg -i /tmp/fpc.deb
   curl --progress-bar -L -o/tmp/fpc-src.deb "https://netcologne.dl.sourceforge.net/project/lazarus/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.8.4/fpc-src_3.0.4-2_amd64.deb" && sudo dpkg -i /tmp/fpc-src.deb /tmp/fpc.deb /tmp/lazarus.deb
 
 # EasyInstall
-  echo "Install Python modules"
+  echo -en "\033[37;1;41m Install Python modules \033[0m"
   sudo easy_install --upgrade pymssql
 
 # Install Chrome
-  echo "Install Chrome"
+  echo -en "\033[37;1;41m Install Chrome \033[0m"
   curl --progress-bar -L -o/tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i /tmp/chrome.deb
 
 # Install Yandex.Browser
-  echo "Install Yandex.Browser"
+  echo -en "\033[37;1;41m Install Yandex.Browser \033[0m"
   curl --progress-bar -L https://cache-mskm901.cdn.yandex.net/download.cdn.yandex.net/browser/yandex/ru/beta/Yandex.deb && sudo dpkg -i /tmp/Yandex.deb
 
 # Install VirtualBox
-  echo "install VirtualBox"
+  echo -en "\033[37;1;41m install VirtualBox \033[0m"
   sudo apt-get -y install libsdl1.2debian
   curl --progress-bar -L -o/tmp/virtualbox.deb https://download.virtualbox.org/virtualbox/5.2.18/virtualbox-5.2_5.2.18-124319~Ubuntu~xenial_amd64.deb && sudo dpkg -i /tmp/virtualbox.deb
 
 # Install Slack
-  echo "install Slack"
+  echo -en "\033[37;1;41m install Slack \033[0m"
   sudo apt-get -y install libappindicator1
   curl --progress-bar -L -o/tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb && sudo dpkg -i /tmp/slack.deb
 
 # Install Skype
-  echo "Install Skype"
+  echo -en "\033[37;1;41m Install Skype \033[0m"
   curl --progress-bar -L -o/tmp/skypeforlinux-64.deb "https://go.skype.com/skypeforlinux-64.deb" && sudo dpkg -i /tmp/skypeforlinux-64.deb
 
 # Install Telegram
 #  wget "https://telegram.org/dl/desktop/linux"
 
 # Install Atom
-  echo "install Atom"
+  echo -en "\033[37;1;41m install Atom \033[37;1;41m"
   curl --progress-bar -L -o/tmp/atom.deb "https://atom.io/download/deb" && sudo dpkg -i /tmp/atom.deb
 
 # Install Powershell
-  echo "Install Powershell"
+  echo -en "\033[37;1;41m Install Powershell \033[37;1;41m"
   sudo apt install liblttng-ust0
   curl --progress-bar -L -o/tmp/powershell.deb https://github.com/PowerShell/PowerShell/releases/download/v6.0.4/powershell_6.0.4-1.ubuntu.16.04_amd64.deb && sudo dpkg -i /tmp/powershell.deb
 
 # Install Docker CE
-  echo "Install Docker CE"
+  echo -en "\033[37;1;41m Install Docker CE \033[0m"
   sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo apt-key fingerprint 0EBFCD88
@@ -78,17 +78,17 @@ function Uinstall() {
   sudo adduser $USER docker
 
 # Install Project Atomic
-  echo "Install Project Atomic"
+  echo -en "\033[37;1;41m Install Project Atomic \033[0m"
   sudo add-apt-repository ppa:projectatomic/ppa
   sudo apt-get update
   sudo apt-get -y podman
 
 # Install Draw.io
-  echo "Install Draw.io"
+  echo -en "\033[37;1;41m Install Draw.io \033[0m"
   curl --progress-bar -L -o/tmp/drawio.deb https://github.com/jgraph/drawio-desktop/releases/download/v8.8.0/draw.io-amd64-8.8.0.deb && sudo dpkg -i /tmp/drawio.deb
 
 # Install Wine
-  echo "install Wine"
+  echo -en "\033[37;1;41m install Wine \033[0m"
   sudo dpkg --add-architecture i386
   wget -nc https://dl.winehq.org/wine-builds/Release.key
   sudo apt-key add Release.key
@@ -97,42 +97,42 @@ function Uinstall() {
   sudo apt-get -y install --install-recommends winehq-stable
 
 # Install Android-studio
-  echo "Install Andrion-studio"
+  echo -en "\033[37;1;41m Install Andrion-studio \033[0m"
   sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
   curl --progress-bar -L -o/tmp/android-studio.zip https://dl.google.com/dl/android/studio/ide-zips/3.2.0.26/android-studio-ide-181.5014246-linux.zip
   sudo unzip /tmp/android-studio.zip -d /opt/
 
 # Tweaks
-  echo "install Tweaks"
+  echo -en "\033[37;1;41m install Tweaks \033[0m"
   sudo apt-get -y install compizconfig-settings-manager
 
 # Teamviewer
-  echo "Install Teamviewer"
+  echo -en "\033[37;1;41m Install Teamviewer \033[0m"
   sudo apt install qtdeclarative5-controls-plugin qml-module-qtquick-controls qml-module-qtquick-dialogs qtdeclarative5-dialogs-plugin
   curl --progress-bar -L -o/tmp/teamviewer_amd64.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && sudo dpkg -i /tmp/teamviewer_amd64.deb
 
 # vSphere Client
-#  echo "vSphere Client"
+#  echo -en "\033[37;1;41m vSphere Client \033[0m"
 #  curl --progress-bar -L -o/home/$USER/.cache/winetricks/msxml3/msxml3.msi "https://files.downloadnow.com/s/software/10/73/16/12/msxml3.msi?token=1539025396_6ee0a7f508da5666cc2c7781551c8e4e&fileName=msxml3.msi"
 #  WINEPREFIX="/home/$USER/wine32" WINEARCH=win32 winetricks vcrun2005 vcrun2008 vcrun2010 vjrun20
 #  curl --progress-bar -L -o/tmp/dotnetfx35.exe "http://download.microsoft.com/download/6/0/f/60fc5854-3cb8-4892-b6db-bd4f42510f28/dotnetfx35.exe"
 #  WINEPREFIX="/home/$USER/wine32" WINEARCH=win32 wine /tmp/dotnetfx35.exe
 #  curl --progress-bar -L -o/tmp/VMware-viclient-all-5.1.0-786111.exe "http://vsphereclient.vmware.com/vsphereclient/7/8/6/1/1/1/VMware-viclient-all-5.1.0-786111.exe"
-  
+
 
 # Install indicators
 ## System sensors
-  echo "Install Sensors indicator"
+  echo -en "\033[37;1;41m Install Sensors indicator \033[0m"
   sudo add-apt-repository ppa:jfi/ppa
   sudo apt-get update
   sudo apt-get -y install psensor
 ## Weather
-  echo "Install Weather indicator"
+  echo -en "\033[37;1;41m Install Weather indicator \033[0m"
   sudo add-apt-repository ppa:atareao/atareao
   sudo apt-get update
   sudo apt-get -y install my-weather-indicator
 ## Yandex disk
-  echo "Install Yandex Disk"
+  echo -en "\033[37;1;41m Install Yandex Disk \033[0m"
   wget http://repo.yandex.ru/yandex-disk/yandex-disk_latest_amd64.deb
   sudo dpkg -i yandex-disk_latest_amd64.deb
   yandex-disk setup
